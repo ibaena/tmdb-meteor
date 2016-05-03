cb8912b9eafb3f4460e3aec63b1a9c347d634201
Ivan <ivan.baena04@gmail.com>
2016-05-03 04:15:29 -0400

    made api call serverside; request on client; set session variable for data

--- client/app/js/Home.js
+++ client/app/js/Home.js
@@ -1,2 +1,32 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {
+  Template
+} from 'meteor/templating';
+import {
+  ReactiveVar
+} from 'meteor/reactive-var';
+
+
+
+Template.Header.onCreated(function() {
+  Meteor.call("getNewMovies", function(err, res) {
+    Session.set('newMovies', res.data);
+    console.log(res);
+  });
+});
+
+
+
+
+Template.Header.helpers({
+  Movies: function() {
+    return Session.get('newMovies');
+
+  }
+});
+
+Template.Header.events({
+  'click .help': () => {
+    console.log('please');
+  }
+
+});
--- server/init.js
+++ server/init.js
@@ -3,3 +3,14 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+var url = "https://api.themoviedb.org/3/movie/550?api_key=3729ffa22dfa780e9abb43dee3074695";
+
+Meteor.methods({
+    getNewMovies: function() {
+        this.unblock();
+        return HTTP.get(url, {
+        });
+    }
+});
